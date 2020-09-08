f8cb3ab3baf26ed7b43fc83f93c15bdbe61c85e1
harugon <info@harugon.com>
2020-09-08 19:30:09 +0900

    clean mediawiki-codesniffer

--- resources/Chart.js
+++ resources/Chart.js
@@ -1,4 +1,3 @@
-
 ( function ( mw ) {
 	mw.loader.using( [ 'moment','ext.chart_js' ] ).done( function () {
 		for( const chartjsId in window.chartjs ) {
@@ -6,9 +5,9 @@
 				const dataJson = window.chartjs[chartjsId];
 				const pel = document.getElementById(chartjsId);
 				const el = document.createElement( 'canvas' );
-				el.setAttribute("id",chartjsId+'_cv');
+				el.setAttribute("id",chartjsId + '_cv');
 				pel.append(el);
-				new Chart(document.getElementById(chartjsId+'_cv'),dataJson)
+				new Chart(document.getElementById(chartjsId + '_cv'),dataJson)
 			}
 		}
 	} )
